af4c81ec5b9658303f86feddaeaf6c647a988c10
ShadowOfDeath25 <152920007+ShadowOfDeath25@users.noreply.github.com>
2024-07-25 10:14:34 +0300

    Created DB connection file
    All you need to do now is import this file and call the connect_db() function to connect to the database :)))

---
+++ DbConnection.py
@@ -0,0 +1,12 @@
+import mysql.connector as db
+import os
+
+
+def connect_db():
+    db_user = os.environ['DB_USER']
+    db_pass = os.environ['DB_PASS']
+    mydb = db.connect(host='localhost',
+                      username=db_user,
+                      password=db_pass,
+                      database="autoshop")
+    return mydb
