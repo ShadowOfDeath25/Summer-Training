3199a35de2121aa9899c03ed629fd8a1fc080631
ShadowOfDeath25 <152920007+ShadowOfDeath25@users.noreply.github.com>
2024-07-30 23:56:40 +0300

    Create Cars.py

---
+++ Cars.py
@@ -0,0 +1,30 @@
+class Cars:
+    manu = ""
+    model = ""
+    year = ""
+    id = ""
+    owner_id = ""
+    engine_capacity = 0
+    horsepower = 0
+    top_speed = 0
+    price = 0
+    photo_path = ""
+    description = ""
+    op_type = ""
+    state = ""
+
+    def __init__(self, manu, model, year, id, owner_id, engine_capacity, horsepower, top_speed, price, photo_path,
+                 description, op_type, state):
+        self.manu = manu
+        self.model = model
+        self.year = year
+        self.id = id
+        self.owner_id = owner_id
+        self.engine_capacity = engine_capacity
+        self.horsepower = horsepower
+        self.top_speed = top_speed
+        self.price = price
+        self.photo_path =photo_path
+        self.description = description
+        self.op_type = op_type
+        self.state = state
