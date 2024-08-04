1832339e6b57cfa3078cb1fb5d0422d443752719
MohamedThaer <125297147+MohamedThaer@users.noreply.github.com>
2024-08-04 22:45:59 +0300

    Add files via upload

--- DatePicker.py
+++ DatePicker.py
@@ -25,25 +25,29 @@ class Date_Picker(Frame):
         self.frame.grid(row=0, column=0, sticky=(W, E, N, S))
         self.frame.config(bg="#FFFFFF")
         # Calendar widgets
-        self.start_cal = Calendar(self.frame, selectmode='day', year=2024, month=8, day=1, date_pattern="d/m/Y")
+        self.time=datetime.strptime(datetime.today().strftime('%Y-%m-%d'), '%Y-%m-%d')
+        self.start_cal = Calendar(self.frame, selectmode='day', year=self.time.year, month=self.time.month, day=self.time.day, date_pattern="d/m/Y")
         self.start_cal.grid(row=0, column=0, padx=10, pady=10)
-        self.end_cal = Calendar(self.frame, selectmode='day', year=2024, month=8, day=1, date_pattern="d/m/Y")
+        self.end_cal = Calendar(self.frame, selectmode='day', year=self.time.year, month=self.time.month, day=self.time.day, date_pattern="d/m/Y")
         self.end_cal.grid(row=0, column=1, padx=10, pady=10)
         # Labels
-        self.start_lbl = ut.create_label(self.win, 90, 50, 'Start Date : \n\n' + "D/M/Y")
-        self.end_lbl = ut.create_label(self.win, 90, 50, 'End Date : \n\n' + "D/M/Y")
+        self.start_lbl = ut.create_label(self.win, 90, 50, 'Start Date : \n\n' + self.start_cal.get_date())
+        self.end_lbl = ut.create_label(self.win, 90, 50, 'End Date : \n\n' + self.end_cal.get_date())
         self.start_lbl.place(x=80, y=220)
         self.end_lbl.place(x=350, y=220)
-        self.price_lbl = ut.create_label(self.win, 90, 50,
+        self.price_lbl = ut.create_label(self.win, 120, 50,
                                          'Days : ' + str(0) + '\n\nPrice : ' + str(0))
-        self.price_lbl.place(x=220, y=280)
+        self.price_lbl.place(x=205, y=280)
         #btn
         self.set_fr, self.set_btn = ut.create_button(self.win, 105, 40, 'red', text='Set Dates')
         self.set_fr.place(x=220, y=220)
         self.set_btn.config(command=lambda Car=car: self.get_dates(Car))
         self.rent_fr, self.rent_btn = ut.create_button(self.win, 105, 40, 'red', text='Rent')
-        self.rent_fr.place(x=220, y=350)
+        self.rent_fr.place(x=80, y=350)
         self.rent_btn.config(command=lambda Car=car,control=controller: self.set_data(control,Car))
+        self.back_fr, self.back_btn = ut.create_button(self.win, 105, 40, 'red', text='Back')
+        self.back_fr.place(x=350, y=350)
+        self.back_btn.config(command=lambda :controller.show_frame("car_rent_view",car))
 
     def get_dates(self , Car):
         start_date = self.start_cal.get_date()
@@ -57,19 +61,25 @@ class Date_Picker(Frame):
         delta = datetime.strptime(end_date, date_format) - datetime.strptime(start_date, date_format)
         price = delta.days * Car.price
         # Price label
-        price_lbl = ut.create_label(self.win, 90, 50, 'Days : ' + str(delta.days) + '\n\nPrice : ' + str(price))
-        price_lbl.place(x=220, y=280)
+        price_lbl = ut.create_label(self.win, 120, 50, 'Days : ' +
+                                    str(delta.days) + '\n\nPrice : ' + str(price))
+        price_lbl.place(x=205, y=280)
 
     def set_data(self, control, Car):
         #curr_db = db.connect_db()
         #cursor = curr_db.cursor()
         start_date = self.start_cal.get_date()
         end_date = self.end_cal.get_date()
-        #date_format = "%d/%m/%Y"
-        #delta = datetime.strptime(end_date, date_format) - datetime.strptime(start_date, date_format)
-        #price = delta.days * Car.price
-        #rental_id = str(uuid.uuid4())
-        #values = (rental_id, Car.id, Car.owner_id, u.current_user.id, start_date, end_date, price)
+        date_format = "%d/%m/%Y"
+        start=datetime.strptime(start_date, date_format)
+        end=datetime.strptime(end_date, date_format)
+        delta = end - start
+        price = delta.days * Car.price
+        rental_id = str(uuid.uuid4())
+        values = (rental_id, Car.id, Car.owner_id, u.current_user.id,
+                  str(start.year)+'-'+str(start.month)+"-"+str(start.day)
+                  ,str(end.year)+'-'+str(end.month)+"-"+str(end.day),
+                  price)
         #cursor.execute(
         #    "INSERT INTO rentals (rental_id, car_id, owner_id,renter_id, rental_date, return_date, price) VALUES (%s,%s,%s,%s,%s,%s,%s)",
         #    values)
